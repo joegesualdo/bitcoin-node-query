3b4c7084f2d0a9129eb3421007f2a6cc0ec63e23
Joe Gesualdo <joegesualdo@gmail.com>
2022-08-10 14:49:48 -0400

    Add more functions

--- examples/example.rs
+++ examples/example.rs
@@ -7,11 +7,21 @@ use std::time::{Duration, SystemTime};
 use std::{env, time::SystemTimeError};
 
 use bitcoin_node_query::{
-    get_average_block_time, get_block_height, get_chain_size, get_current_difficulty_epoch,
-    get_difficulty, get_estimated_hash_rate_per_second_for_block_since_last_difficulty_change,
-    get_estimated_hash_rate_per_second_for_last_2016_blocks, get_mempool_transactions_count,
-    get_time_since_last_block_in_seconds, get_total_fee_for_24_hours, get_total_transactions_count,
-    get_tps_for_last_30_days, get_transactions_count_over_last_30_days, get_utxo_set_size,
+    get_average_block_time_for_last_2016_blocks,
+    get_average_block_time_for_since_last_difficulty_adjustement,
+    get_average_fees_per_block_over_last_2016_blocks,
+    get_average_fees_per_block_over_last_24_hours, get_block_height,
+    get_block_height_of_last_difficulty_adjustment, get_block_subsidy_of_most_recent_block,
+    get_blocks_count_until_retarget, get_blocks_mined_over_last_24_hours_count, get_chain_size,
+    get_current_difficulty_epoch, get_difficulty,
+    get_estimated_hash_rate_per_second_for_block_since_last_difficulty_change,
+    get_estimated_hash_rate_per_second_for_last_2016_blocks,
+    get_estimated_hash_rate_per_second_for_last_epoch, get_estimated_seconds_until_retarget,
+    get_fees_as_a_percent_of_reward_for_last_2016_blocks,
+    get_fees_as_a_percent_of_reward_for_last_24_hours, get_mempool_transactions_count,
+    get_time_since_last_block_in_seconds, get_total_fee_for_24_hours, get_total_money_supply,
+    get_total_transactions_count, get_tps_for_last_30_days,
+    get_transactions_count_over_last_30_days, get_utxo_set_size,
 };
 
 fn client(url: &str, user: &str, pass: &str) -> Result<Client, simple_http::Error> {
@@ -44,13 +54,20 @@ fn main() {
         format_duration(seconds_since_last_block)
     );
 
-    let average_seconds_per_block = get_average_block_time(&client);
+    let average_seconds_per_block_last_2016_blocks =
+        get_average_block_time_for_last_2016_blocks(&client);
     println!(
         "AVERAGE BLOCK TIME (2016): {}",
-        format_duration(average_seconds_per_block as i64)
+        format_duration(average_seconds_per_block_last_2016_blocks as i64)
+    );
+    let average_seconds_per_block_since_last_difficulty_adjustment =
+        get_average_block_time_for_since_last_difficulty_adjustement(&client);
+    println!(
+        "AVERAGE BLOCK TIME (SINCE LAST_DIFFICULTY ADJUSTMENT): {}",
+        format_duration(average_seconds_per_block_since_last_difficulty_adjustment as i64)
     );
 
-    // Errors out
+    // Errors out because: https://github.com/apoelstra/rust-jsonrpc/issues/67
     // let total_money_supply = get_total_money_supply(&client);
     // println!("TOTAL MONEY SUPPLY: {:#?}", total_money_supply);
 
@@ -58,7 +75,7 @@ fn main() {
     let chain_size_in_gbs = chain_size as f64 / 1_000_000_000.0;
     println!("CHAIN SIZE: {:#?}GB", chain_size_in_gbs);
 
-    // Errors out
+    // Errors out because: https://github.com/apoelstra/rust-jsonrpc/issues/67
     // let utxo_set_size = get_utxo_set_size(&client);
     // println!("UTXO SET SIZE: {:#?}", utxo_set_size);
 
@@ -75,20 +92,18 @@ fn main() {
     );
 
     // takes a long time
-    // TODO: Uncomment
-    // let transactions_count_over_last_30_days = get_transactions_count_over_last_30_days(&client);
-    // println!(
-    //     "TRANSACTIONS COUNT OVER LAST 30 DAYS: {:#?}",
-    //     transactions_count_over_last_30_days
-    // );
+    let transactions_count_over_last_30_days = get_transactions_count_over_last_30_days(&client);
+    println!(
+        "TRANSACTIONS COUNT OVER LAST 30 DAYS: {:#?}",
+        transactions_count_over_last_30_days
+    );
 
     // takes a long time
-    // TODO: Uncomment
-    //let total_fee_for_24_hours = get_total_fee_for_24_hours(&client);
-    //println!(
-    //    "TOTAL FEE FOR LAST 24 hours: {:#?}",
-    //    total_fee_for_24_hours as f64 / 100_000_000.0
-    //);
+    let total_fee_for_24_hours = get_total_fee_for_24_hours(&client);
+    println!(
+        "TOTAL FEE FOR LAST 24 hours: {:#?} btc",
+        total_fee_for_24_hours as f64 / 100_000_000.0
+    );
 
     let difficulty = get_difficulty(&client);
     let trillion: u64 = 1_000_000_000_000;
@@ -97,6 +112,12 @@ fn main() {
 
     let current_difficulty_epoch = get_current_difficulty_epoch(&client);
     println!("CURRENT EPOCH: {:?}", current_difficulty_epoch);
+    let block_height_of_last_difficulty_adjustment =
+        get_block_height_of_last_difficulty_adjustment(&client);
+    println!(
+        "BLOCK HEIGHT OF LAST DIFFICULTY: {:?}",
+        block_height_of_last_difficulty_adjustment
+    );
 
     let mempool_transaction_count = get_mempool_transactions_count(&client);
     println!("MEMPOOL TRANSACTION COUNT: {:?}", mempool_transaction_count);
@@ -112,5 +133,62 @@ fn main() {
     println!(
         "ESTIMATED HASH RATE FOR LAST 2016 BLOCKS: {}",
         hash_rate_for_last_2016_blocks
+    );
+    let hash_rate_for_last_epoch = get_estimated_hash_rate_per_second_for_last_epoch(&client);
+    println!(
+        "ESTIMATED HASH RATE FOR LAST EPOCH: {}",
+        hash_rate_for_last_epoch
+    );
+    let blocks_till_difficulty_adjustment = get_blocks_count_until_retarget(&client);
+    println!(
+        "BLOCKS TILL DIFFICULTY ADJUSTMENT: {}",
+        blocks_till_difficulty_adjustment
+    );
+    let estimated_seconds_until_retarget = get_estimated_seconds_until_retarget(&client);
+    println!(
+        "ESTIMATED SECONDS UNTIL RETARGET: {}",
+        estimated_seconds_until_retarget
+    );
+
+    // takes a long time
+    let blocks_mined_over_last_24_hours = get_blocks_mined_over_last_24_hours_count(&client);
+    println!(
+        "BLOCKS MINED OVER LAST 24 HOURS: {}",
+        blocks_mined_over_last_24_hours
+    );
+    // takes a long time
+    let average_fees_per_block_over_last_24_hours =
+        get_average_fees_per_block_over_last_24_hours(&client);
+    println!(
+        "AVERAGE FEES PER BLOCK OVER LAST 24 HOURS: {} btc",
+        average_fees_per_block_over_last_24_hours as f64 / 100_000_000.0
+    );
+    // takes a long time
+    let average_fees_per_block_over_last_2016_blocks =
+        get_average_fees_per_block_over_last_2016_blocks(&client);
+    println!(
+        "AVERAGE FEES PER BLOCK OVER LAST 2016 BLOCKS: {} btc",
+        average_fees_per_block_over_last_2016_blocks as f64 / 100_000_000.0
+    );
+
+    //takes a long time
+    let fees_as_a_percent_of_reward_for_last_24_hours_ =
+        get_fees_as_a_percent_of_reward_for_last_24_hours(&client);
+    println!(
+        "FEES AS A PERCENT OF REWARD OVER THE LAST 24 HOURS: {}",
+        fees_as_a_percent_of_reward_for_last_24_hours_
+    );
+    // takes a long time
+    let fees_as_a_percent_of_reward_for_last_2016_blocks =
+        get_fees_as_a_percent_of_reward_for_last_2016_blocks(&client);
+    println!(
+        "FEES AS A PERCENT OF REWARD OVER THE LAST 2016 BLOCKS: {}",
+        fees_as_a_percent_of_reward_for_last_2016_blocks
+    );
+
+    let block_subsidy_of_most_recent_block = get_block_subsidy_of_most_recent_block(&client);
+    println!(
+        "BLOCK SUBSIDY OF MOST RECENT BLOCK: {} btc",
+        block_subsidy_of_most_recent_block as f64 / 100_000_000.0
     );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 use bitcoind_request::command::{
     get_best_block_hash::GetBestBlockHashCommand,
     get_block::{
-        GetBlockCommand, GetBlockCommandResponse, GetBlockCommandTransactionResponse,
+        self, GetBlockCommand, GetBlockCommandResponse, GetBlockCommandTransactionResponse,
         GetBlockCommandVerbosity,
     },
     get_block_count::{self, GetBlockCountCommand},
@@ -28,12 +28,25 @@ use bitcoind_request::command::{
 
 use bitcoind_request::{Blockhash, BlockhashHexEncoded};
 
-use chrono::{DateTime, Duration, TimeZone, Utc};
+use chrono::{DateTime, Duration, TimeZone, Timelike, Utc};
 use jsonrpc::simple_http::{self, SimpleHttpTransport};
 use jsonrpc::Client;
 use std::{env, time::SystemTimeError};
 
+const BLOCKS_PER_DIFFICULTY_PERIOD: u64 = 2016;
+
 pub struct Seconds(pub i64);
+
+fn timestamp_is_from_more_than_24_hours_ago(timestamp: i64) -> bool {
+    let hour_window_to_calculate_for = 24;
+
+    let current_datetime = chrono::offset::Utc::now();
+    let window_to_cacluate_duration_in_seconds =
+        Duration::seconds(60 * 60 * hour_window_to_calculate_for);
+    let datetime_24_hours_ago = current_datetime - window_to_cacluate_duration_in_seconds;
+    let datetime_of_block = Utc.timestamp(timestamp as i64, 0);
+    datetime_of_block < datetime_24_hours_ago
+}
 
 pub fn get_block_height(client: &Client) -> u64 {
     let block_count = GetBlockCountCommand::new().call(client);
@@ -54,12 +67,52 @@ pub fn get_time_since_last_block_in_seconds(client: &Client) -> i64 {
     difference.num_seconds()
 }
 
-pub fn get_average_block_time(client: &Client) -> u64 {
-    let blocks_to_calculate = 2016;
-    let chain_tx_stats = GetChainTxStatsCommand::new()
-        .set_n_blocks(2016)
-        .call(client);
-    let average_seconds_per_block = chain_tx_stats.window_interval / blocks_to_calculate;
+pub fn get_average_block_time_for_last_2016_blocks(client: &Client) -> u64 {
+    let block_height = GetBlockCountCommand::new().call(client);
+    let block_stats_response =
+        GetBlockStatsCommand::new(TargetBlockArgument::Height(block_height.0)).call(client);
+    let time_of_most_recent_block = match block_stats_response {
+        GetBlockStatsCommandResponse::AllStats(response) => response.time,
+        GetBlockStatsCommandResponse::SelectiveStats(response) => response.time.unwrap(),
+    };
+
+    let block_stats_response_for_block_2016_old =
+        GetBlockStatsCommand::new(TargetBlockArgument::Height(block_height.0 - 2016)).call(client);
+    let time_of_block_2016_old = match block_stats_response_for_block_2016_old {
+        GetBlockStatsCommandResponse::AllStats(response) => response.time,
+        GetBlockStatsCommandResponse::SelectiveStats(response) => response.time.unwrap(),
+    };
+
+    let duration = time_of_most_recent_block - time_of_block_2016_old;
+    let average_seconds_per_block = duration / 2016 as u64;
+    average_seconds_per_block
+}
+pub fn get_average_block_time_for_since_last_difficulty_adjustement(client: &Client) -> u64 {
+    let block_height = GetBlockCountCommand::new().call(client);
+    let block_stats_response =
+        GetBlockStatsCommand::new(TargetBlockArgument::Height(block_height.0)).call(client);
+    let time_of_most_recent_block = match block_stats_response {
+        GetBlockStatsCommandResponse::AllStats(response) => response.time,
+        GetBlockStatsCommandResponse::SelectiveStats(response) => response.time.unwrap(),
+    };
+
+    let block_height_of_last_difficulty_adjustment =
+        get_block_height_of_last_difficulty_adjustment(client);
+    let block_stats_response_for_last_difficulty_ajustment_block = GetBlockStatsCommand::new(
+        TargetBlockArgument::Height(block_height_of_last_difficulty_adjustment),
+    )
+    .call(client);
+    let time_of_last_difficulty_adjustment_block =
+        match block_stats_response_for_last_difficulty_ajustment_block {
+            GetBlockStatsCommandResponse::AllStats(response) => response.time,
+            GetBlockStatsCommandResponse::SelectiveStats(response) => response.time.unwrap(),
+        };
+
+    let blocks_since_last_retarget =
+        BLOCKS_PER_DIFFICULTY_PERIOD as f64 - get_blocks_count_until_retarget(client);
+
+    let duration = time_of_most_recent_block - time_of_last_difficulty_adjustment_block;
+    let average_seconds_per_block = duration / blocks_since_last_retarget as u64;
     average_seconds_per_block
 }
 
@@ -95,6 +148,7 @@ pub fn get_tps_for_last_30_days(client: &Client) -> f64 {
     tps
 }
 
+// takes a long time
 pub fn get_transactions_count_over_last_30_days(client: &Client) -> u64 {
     let chain_tx_stats = GetChainTxStatsCommand::new().call(client);
     chain_tx_stats.window_tx_count
@@ -110,6 +164,16 @@ pub fn get_total_fee_for_block_at_height(client: &Client, height: u64) -> u64 {
     };
     total_fee
 }
+fn get_subsidy_for_block_at_height(client: &Client, height: u64) -> u64 {
+    let block_stats = GetBlockStatsCommand::new(TargetBlockArgument::Height(height))
+        .add_selective_stats(vec![StatsArgumentChoices::Subsidy])
+        .call(client);
+    let subsidy = match block_stats {
+        GetBlockStatsCommandResponse::AllStats(response) => response.subsidy,
+        GetBlockStatsCommandResponse::SelectiveStats(response) => response.subsidy.unwrap(),
+    };
+    subsidy
+}
 
 fn get_timestamp_of_block_at_height(client: &Client, height: u64) -> u64 {
     let block_stats = GetBlockStatsCommand::new(TargetBlockArgument::Height(height))
@@ -122,19 +186,9 @@ fn get_timestamp_of_block_at_height(client: &Client, height: u64) -> u64 {
     time
 }
 
+// takes a long time
 pub fn get_total_fee_for_24_hours(client: &Client) -> u64 {
     let last_block_height = get_block_height(&client);
-
-    fn timestamp_is_from_more_than_24_hours_ago(timestamp: i64) -> bool {
-        let hour_window_to_calculate_for = 24;
-
-        let current_datetime = chrono::offset::Utc::now();
-        let window_to_cacluate_duration_in_seconds =
-            Duration::seconds(60 * 60 * hour_window_to_calculate_for);
-        let datetime_24_hours_ago = current_datetime - window_to_cacluate_duration_in_seconds;
-        let datetime_of_block = Utc.timestamp(timestamp as i64, 0);
-        datetime_of_block < datetime_24_hours_ago
-    }
 
     let mut total_fee = 0;
     let mut current_block_height = last_block_height;
@@ -161,8 +215,13 @@ pub fn get_difficulty(client: &Client) -> f64 {
 
 pub fn get_current_difficulty_epoch(client: &Client) -> u64 {
     let block_count = get_block_height(client);
-    let epoch = (block_count / 2016) + 1;
+    let epoch = (block_count / BLOCKS_PER_DIFFICULTY_PERIOD) + 1;
     epoch
+}
+pub fn get_block_height_of_last_difficulty_adjustment(client: &Client) -> u64 {
+    let last_epoch = get_current_difficulty_epoch(client) - 1;
+    let block_height_of_last_difficulty_adjustment = last_epoch * 2016;
+    block_height_of_last_difficulty_adjustment
 }
 
 pub fn get_mempool_transactions_count(client: &Client) -> u64 {
@@ -179,11 +238,159 @@ pub fn get_estimated_hash_rate_per_second_for_block_since_last_difficulty_change
         .call(client);
     hash_rate.0
 }
+
 pub fn get_estimated_hash_rate_per_second_for_last_2016_blocks(client: &Client) -> f64 {
+    let blocks_to_calculate = 2016;
     let hash_rate = GetNetworkHashPsCommand::new()
         .set_n_blocks(
-            bitcoind_request::command::get_network_hash_ps::BlocksToIncludeArg::NBlocks(2016),
+            bitcoind_request::command::get_network_hash_ps::BlocksToIncludeArg::NBlocks(
+                blocks_to_calculate,
+            ),
         )
         .call(client);
     hash_rate.0
+}
+pub fn get_estimated_hash_rate_per_second_for_last_epoch(client: &Client) -> f64 {
+    let block_height_of_last_difficulty_adjustment =
+        get_block_height_of_last_difficulty_adjustment(client);
+    let hash_rate = GetNetworkHashPsCommand::new()
+        .set_n_blocks(
+            bitcoind_request::command::get_network_hash_ps::BlocksToIncludeArg::NBlocks(
+                BLOCKS_PER_DIFFICULTY_PERIOD,
+            ),
+        )
+        .set_height(
+            bitcoind_request::command::get_network_hash_ps::HeightArg::Height(
+                block_height_of_last_difficulty_adjustment,
+            ),
+        )
+        .call(client);
+    hash_rate.0
+}
+
+pub fn get_blocks_count_until_retarget(client: &Client) -> f64 {
+    let block_count = get_block_height(client);
+    let percent_of_epoch_complete: f64 =
+        (block_count as f64 / BLOCKS_PER_DIFFICULTY_PERIOD as f64) % 1.0;
+    let percent_of_epoch_to_go: f64 = 1.0 - percent_of_epoch_complete;
+    let blocks_until_retarget = percent_of_epoch_to_go * (BLOCKS_PER_DIFFICULTY_PERIOD as f64);
+    blocks_until_retarget
+}
+
+pub fn get_estimated_seconds_until_retarget(client: &Client) -> f64 {
+    // TODO: Could we get a more accurate prediction if we use average block times in the current
+    // epoch?
+    // let average_block_time_for_current_epoch =
+    get_average_block_time_for_since_last_difficulty_adjustement(client);
+    let blocks_count_until_retarget = get_blocks_count_until_retarget(client);
+    10.0 * 60.0 * blocks_count_until_retarget
+}
+
+// takes a long time
+pub fn get_blocks_mined_over_last_24_hours_count(client: &Client) -> u64 {
+    let block_count = get_block_height(client);
+
+    // defaults to current time
+    let mut next_block_timestamp: u64 = get_timestamp_of_block_at_height(client, block_count);
+    let mut traversed_blocks_count = 0;
+    // Calculate fee while the blocktime is within the 24 hour window.
+    while !timestamp_is_from_more_than_24_hours_ago(next_block_timestamp as i64) {
+        traversed_blocks_count = traversed_blocks_count + 1;
+
+        let block_height_to_calculate = block_count - traversed_blocks_count;
+        let time = get_timestamp_of_block_at_height(client, block_height_to_calculate);
+
+        next_block_timestamp = time as u64;
+    }
+
+    traversed_blocks_count
+}
+
+// takes a long time
+pub fn get_average_fees_per_block_over_last_24_hours(client: &Client) -> u64 {
+    let block_count = get_block_height(client);
+
+    // defaults to current time
+    let mut next_block_timestamp: u64 = get_timestamp_of_block_at_height(client, block_count);
+    let mut traversed_blocks_count = 0;
+    let mut total_fee = 0;
+    // Calculate fee while the blocktime is within the 24 hour window.
+    while !timestamp_is_from_more_than_24_hours_ago(next_block_timestamp as i64) {
+        let block_fee =
+            get_total_fee_for_block_at_height(client, block_count - traversed_blocks_count);
+        traversed_blocks_count = traversed_blocks_count + 1;
+        total_fee = total_fee + block_fee;
+
+        let next_block_height_to_calculate = block_count - traversed_blocks_count;
+        let time = get_timestamp_of_block_at_height(client, next_block_height_to_calculate);
+
+        next_block_timestamp = time as u64;
+    }
+
+    total_fee / traversed_blocks_count
+}
+
+// takes a long time
+pub fn get_average_fees_per_block_over_last_2016_blocks(client: &Client) -> u64 {
+    let block_count = get_block_height(client);
+    // defaults to current time
+    let mut total_fee = 0;
+
+    for i in 0..=2016 {
+        // Calculate fee while the blocktime is within the 24 hour window.
+        let block_fee = get_total_fee_for_block_at_height(client, block_count - i);
+        total_fee = total_fee + block_fee;
+    }
+
+    total_fee / 2016
+}
+
+pub fn get_fees_as_a_percent_of_reward_for_last_24_hours(client: &Client) -> f64 {
+    let block_count = get_block_height(client);
+
+    // defaults to current time
+    let mut next_block_timestamp: u64 = get_timestamp_of_block_at_height(client, block_count);
+    let mut traversed_blocks_count = 0;
+    let mut total_fee = 0.0;
+    let mut total_subsidy = 0.0;
+    // Calculate fee while the blocktime is within the 24 hour window.
+    while !timestamp_is_from_more_than_24_hours_ago(next_block_timestamp as i64) {
+        let block_fee =
+            get_total_fee_for_block_at_height(client, block_count - traversed_blocks_count);
+        let block_subsidy =
+            get_subsidy_for_block_at_height(client, block_count - traversed_blocks_count);
+        traversed_blocks_count = traversed_blocks_count + 1;
+        total_fee = total_fee as f64 + block_fee as f64;
+        total_subsidy = total_subsidy as f64 + block_subsidy as f64;
+
+        let next_block_height_to_calculate = block_count - traversed_blocks_count;
+        let time = get_timestamp_of_block_at_height(client, next_block_height_to_calculate);
+
+        next_block_timestamp = time as u64;
+    }
+
+    total_fee / (total_subsidy + total_fee)
+}
+
+pub fn get_fees_as_a_percent_of_reward_for_last_2016_blocks(client: &Client) -> f64 {
+    let block_count = get_block_height(client);
+    // defaults to current time
+    let mut total_fee = 0.0;
+    let mut total_subsidy = 0.0;
+
+    for i in 0..=2016 {
+        // Calculate fee while the blocktime is within the 24 hour window.
+        let block_fee = get_total_fee_for_block_at_height(client, block_count - i);
+        let block_subsidy = get_subsidy_for_block_at_height(client, block_count);
+        total_fee = total_fee as f64 + block_fee as f64;
+        total_subsidy = total_subsidy as f64 + block_subsidy as f64;
+    }
+
+    total_fee / (total_subsidy + total_fee)
+}
+
+pub fn get_block_subsidy_of_most_recent_block(client: &Client) -> u64 {
+    let block_count = get_block_height(client);
+    let block_subsidy = get_subsidy_for_block_at_height(client, block_count);
+    block_subsidy
 }
